2ceabee7f3206e841cca3ee46e3a634bef866816
Venue Star <Venue Star@MSI.mshome.net>
2021-01-06 14:44:39 +0300

    First commit from ashik

--- backend/controllers/CrmManagerController.php
+++ backend/controllers/CrmManagerController.php
@@ -199,7 +199,27 @@ class CrmManagerController extends Controller {
     }
 
     function callAPI($method, $url, $data) {
+        $curl = curl_init();
 
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://www.smetron.com/casper/api/Devices?access_token=SID-VXPH04WRFZETQTR8VM05Y8QSPCNKJAT06012021111742317U24',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
+        exit;
         $access_token = $this->GetAccessToken();
 
         if ($access_token != '') {
