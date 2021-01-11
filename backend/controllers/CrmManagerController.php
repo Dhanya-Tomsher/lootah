18528160545ed73c6b32b81bc8225554231264e2
Venue Star <Venue Star@MSI.mshome.net>
2021-01-11 21:41:50 +0300

    transaction commit v1

--- backend/controllers/CrmManagerController.php
+++ backend/controllers/CrmManagerController.php
@@ -121,6 +121,24 @@ class CrmManagerController extends Controller {
      * @throws NotFoundHttpException if the model cannot be found
      */
     public function actionUpdate($id) {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://www.smetron.com/casper/api/Auth?username=tutorial&password=1215',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
+        exit;
         $model = $this->findModel($id);
         $params = [];
         if ($model != NULL) {
