ec31220d1834668422fca3fe2eea2fb2f30ceb4c
Venue Star <Venue Star@MSI.mshome.net>
2021-01-11 23:47:11 +0300

    transaction commit v1

--- backend/controllers/CrmManagerController.php
+++ backend/controllers/CrmManagerController.php
@@ -241,7 +241,7 @@ class CrmManagerController extends Controller {
                     break;
                 default:
                     if ($data != NULL)
-                        $url = sprintf("%s?%s", $post_url, http_build_query($data));
+                        $url = sprintf("%s?%s", $post_url, http_build_query(json_decode($data)));
 
                     //$url = $post_url . "/" . http_build_query(json_decode($data));
                     else
